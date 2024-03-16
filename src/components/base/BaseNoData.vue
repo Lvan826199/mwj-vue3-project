4c535324e7a19ca2b90243d3fc5045c19a4e39cc
Lvan826199 <Lvan826199@163.com>
2024-03-17 03:36:30 +0800

    修改样式

--- src/components/base/BaseNoData.vue
+++ src/components/base/BaseNoData.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-center-center h-full font-size-base text-color-grey">
-    <slot>暂无数据</slot>
+    <slot>暂无数据BaseNoData</slot>
   </div>
 </template>
 <style lang="scss" scoped></style>
